46ebb250e0adefa79799745b2adc336e5305ba48
Bombastik1 <plutenko.ay@gmail.com>
2023-12-29 15:27:42 +0200

    Добавьте файлы проекта.

---
+++ Lab_15(4)_Plutenko/Lab_15(4)_Plutenko.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h> 
+#include <limits.h> 
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "");
+    int n = 10;
+    int i;
+    float a = 0;
+    int* array = (int*)malloc(n * sizeof(int));
+
+    srand((unsigned int)time(NULL));
+    for (i = 0; i < n; i++) {
+        array[i] = rand() % 201 - 100;
+        a += array[i];
+        printf("%d\n", array[i]);
+    }
+    printf("Сума елементiв масиву: %f\n", a);
+    return 0;
+}
